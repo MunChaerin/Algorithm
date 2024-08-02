6d5ca9b9b475d698a2eae15cf82d7e8b863f9284
MunChaerin <164905124+MunChaerin@users.noreply.github.com>
2024-08-02 23:30:04 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 72 ms, Memory: 33468 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,19 @@
+import sys
+s = list(sys.stdin.readline().rstrip())
+i, start = 0, 0
+while i < len(s):
+    if s[i]=='<':
+        i+=1
+        while s[i]!='>':
+            i+=1
+        i+=1
+    elif s[i].isalnum():
+        start=i
+        while i<len(s) and s[i].isalnum():
+            i+=1
+        tmp = s[start:i]
+        tmp.reverse()
+        s[start:i]=tmp
+    else:
+        i+=1
+print(''.join(s))
