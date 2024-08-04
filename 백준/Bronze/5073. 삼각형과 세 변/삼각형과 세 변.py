24693792824630e7bac3e808c840d58d1b333b78
MunChaerin <164905124+MunChaerin@users.noreply.github.com>
2024-08-04 22:16:13 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
@@ -1,17 +1,4 @@
 while True:
-    a,b,c = map(int,input().split())
-    if a==b==c==0 :
-        break
-    else:
-        if max(a,b,c) == a:
-            if a>=b+c : print('Invalid')
-            else:    
-                print('Equilateral' if a==b==c else 'Isosceles' if (a==b and a!=c) or (a==c and a!=b) or (c==b and a!=c) else 'Scalene')
-        elif max(a,b,c) == b:
-            if b>=a+c : print('Invalid')
-            else:    
-                print('Equilateral' if a==b==c else 'Isosceles' if (a==b and a!=c) or (a==c and a!=b) or (c==b and a!=c) else 'Scalene')
-        elif max(a,b,c) == c:
-            if c>=b+a : print('Invalid')
-            else:    
-                print('Equilateral' if a==b==c else 'Isosceles' if (a==b and a!=c) or (a==c and a!=b) or (c==b and a!=c) else 'Scalene')
+    a= list(map(int,input().split()))
+    if sum(a)==0 : break
+    print(('Invalid','Equilateral','Isosceles','Scalene')[(max(a)<sum(a)-max(a))*len({*a})])
