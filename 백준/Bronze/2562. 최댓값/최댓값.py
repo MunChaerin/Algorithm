3e6e877148b6e273878bd03cec909754da30e50a
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2024-09-28 20:39:46 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,11 +1,2 @@
-arr=[]
-max_n = 0
-max_i = 0
-for i in range(9):
-    n = int(input())
-    arr.append(n)
-    if(max_n<n):
-        max_n = n
-        max_i = i+1
-print(max_n)
-print(max_i)
+n = [int(input()) for i in range(9)]
+print(max(n), n.index(max(n))+1)
