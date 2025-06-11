85ee42bb00f720503e7dc5d6eb7cad61f8f83dae
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-06-11 13:45:47 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int n) {
-        for(int i = 1; i<n+1; i++){
-            if((i*7)>=n){
-                return i;
-            }
-        }
-        return -1;
+        return (n+6)/7;
     }
 }
