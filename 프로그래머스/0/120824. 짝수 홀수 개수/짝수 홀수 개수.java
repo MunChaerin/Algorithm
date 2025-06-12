7b46ee3da0bd584b45c086348d84c85f7f29bedc
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-06-12 14:37:52 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,12 +1,8 @@
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[2];
-        for(int num : num_list){
-            if(num%2==0){
-                answer[0]+=1;
-            }else{
-                answer[1]+=1;
-            }
+        for(int i=0;i<num_list.length;i++){
+            answer[num_list[i]%2]++;
         }
         return answer;
     }
