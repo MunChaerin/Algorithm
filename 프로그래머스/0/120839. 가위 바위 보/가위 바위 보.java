a01a411619f4bbe54a7358ebcdf39a071cd015de
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-06-13 15:57:03 +0900

    [level 0] Title: 가위 바위 보, Time: 3.20 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        String[] rspList = rsp.split("");
+        
+        for(String str : rspList){
+            switch(str){
+                case "0":
+                    answer += "5";
+                    break;
+                case "2":
+                    answer += "0";
+                    break;
+                case "5":
+                    answer += "2";
+                    break;
+            }
+        }
+        return answer;
+    }
+}
