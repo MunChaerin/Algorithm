5bb81c80296bd5117713e51f0fc59490c1e64bef
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-06-16 13:45:18 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.java
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -2,10 +2,8 @@ import java.util.*;
 class Solution {
     public int[][] solution(int[] num_list, int n) {
         int[][] answer = new int[num_list.length/n][n];
-        int start_index =0;
-        for(int i = 0; i<num_list.length/n;i++){
-            answer[i] = Arrays.copyOfRange(num_list,start_index,start_index+n);
-            start_index+=n;
+        for(int i = 0; i<num_list.length;i++){
+            answer[i/n][i%n] = num_list[i];
         }
         return answer;
     }
