39499ef6d51ac0b3232afcc3dba6b32a7d4a89e9
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-06-16 16:16:38 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -1,21 +1,12 @@
 class Solution {
     public int[] solution(int[] numbers, String direction) {
-        switch(direction){
-            case "left":
-                int temp1 = numbers[0];
-                for(int i = 0; i<numbers.length-1; i++){
-                    numbers[i] = numbers[i+1];
-                }
-                numbers[numbers.length-1] = temp1;
-                break;
-            case "right":
-                int temp2 = numbers[numbers.length-1];
-                for(int i = numbers.length-1; i>0; i--){
-                    numbers[i] = numbers[i-1];
-                }
-                numbers[0] = temp2;
-                break;
-        }
-        return numbers;
+        int length = numbers.length;
+        int[] answer = new int[length];
+        int direct = "right".equals(direction)?1:-1;
+        
+        for(int i =0; i<length; i++)
+            answer[i] = numbers[(i-direct+length)%length];
+        
+        return answer;
     }
 }
