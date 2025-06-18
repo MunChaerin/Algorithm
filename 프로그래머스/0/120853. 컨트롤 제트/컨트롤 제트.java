adecd20c62c049f90783195e36323ffaaa31e0f2
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-06-18 10:27:41 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.37 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        Stack<String> stack = new Stack<>();
+        
+        for(String str : s.split(" ")){
+            if(str.equals("Z")){
+                stack.pop();
+            }else{
+                stack.push(str);
+            }
+        }
+        
+        while(!stack.isEmpty()){
+            answer += Integer.parseInt(stack.pop());
+        }
+        
+        return answer;
+    }
+}
