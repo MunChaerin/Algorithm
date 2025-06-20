b1a037f3ce256f73e0cd040c5cc9613f0b7896df
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-06-20 11:04:04 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.51 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -1,12 +1,8 @@
 import java.util.*;
 class Solution {
     public String solution(String my_string) {
-        char[] arr = my_string.toCharArray();
-        for(int i=0; i<arr.length;i++){
-            if(Character.isUpperCase(arr[i])) arr[i]=Character.toLowerCase(arr[i]);
-        }
+        char[] arr = my_string.toLowerCase().toCharArray();
         Arrays.sort(arr);
-        String answer = String.valueOf(arr);
-        return answer;
+        return new String(arr);
     }
 }
