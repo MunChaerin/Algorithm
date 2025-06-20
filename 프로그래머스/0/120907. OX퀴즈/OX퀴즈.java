474c95599624caf1848c6cda34714a8ac251dc8d
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-06-20 13:30:32 +0900

    [level 0] Title: OX퀴즈, Time: 0.16 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -3,17 +3,7 @@ class Solution {
         
         for(int i=0; i<quiz.length;i++){
             String[] temp = quiz[i].split(" ");
-            int result = Integer.parseInt(temp[0]);
-            int num2 = Integer.parseInt(temp[2]);
-            
-            switch(temp[1]){
-                case "+":
-                    result += num2;
-                    break;
-                case "-":
-                    result -= num2;
-                    break;
-            }
+            int result = Integer.parseInt(temp[0])+(Integer.parseInt(temp[2])*(temp[1].equals("+")?1:-1));
             
             quiz[i] = result==Integer.parseInt(temp[4])?"O":"X";
         }
