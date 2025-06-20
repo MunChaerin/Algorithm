fcc1d67495fa2240a01a05525d72161d67eca57f
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-06-20 13:35:57 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.52 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -2,8 +2,6 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] numbers) {
         Arrays.sort(numbers);
-        int[] answer = {numbers[0]*numbers[1],numbers[numbers.length-1]*numbers[numbers.length-2]};
-        Arrays.sort(answer);
-        return answer[answer.length-1];
+        return Math.max(numbers[0]*numbers[1],numbers[numbers.length-1]*numbers[numbers.length-2]);
     }
 }
