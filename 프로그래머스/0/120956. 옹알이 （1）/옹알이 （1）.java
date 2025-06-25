d87129bb8f05a7300dc2b56ead101fcba284726e
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-06-25 14:10:50 +0900

    [level 0] Title: 옹알이 (1), Time: 13.55 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,8 +1,6 @@
-import java.util.*;
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        List<String> str = new ArrayList<>();
         for(String s : babbling){
             if(s.replaceAll("aya|ye|woo|ma", "").length()==0) answer++;
         }
