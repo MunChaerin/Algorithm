4df0fab0040c21bde0771bba2c723c02f0e3cf12
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-06-27 15:26:45 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,12 +1,6 @@
 class Solution {
     public int solution(String A, String B) {
-        if (A.equals(B)) return 0;
-
-        for (int i = 1; i <= A.length(); i++) {
-            A = A.charAt(A.length() - 1) + A.substring(0, A.length() - 1);
-            if (A.equals(B)) return i;
-        }
-
-        return -1;
+        String temp = B.repeat(3);
+        return temp.indexOf(A);
     }
 }
