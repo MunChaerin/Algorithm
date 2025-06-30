75b6f01850ed7c1359f20372ced421aac2bd814b
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-06-30 15:18:18 +0900

    [level 0] Title: 더 크게 합치기, Time: 3.31 ms, Memory: 93.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int a, int b) {
+        String s1 = Integer.toString(a);
+        String s2 = Integer.toString(b);
+        return Math.max(Integer.parseInt(s1+s2),Integer.parseInt(s2+s1));
+    }
+}
