44aa6927339cf6591c08049adcf169d7687ec489
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-06-30 15:06:02 +0900

    [level 0] Title: 할 일 목록, Time: 12.71 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,10 +1,10 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        List<String> answer = new ArrayList<>();
-        for(int i=0; i<finished.length;i++){
-            if(!finished[i]) answer.add(todo_list[i]);
+        String str = "";
+        for(int i=0;i<finished.length;i++){
+            str = finished[i]==false?str+todo_list[i]+",":str;
         }
-        return answer.toArray(new String[0]);
+        return str.split(",");
     }
 }
