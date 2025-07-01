9b51e40a7e79dd3eab535635184c9dc5eb70f244
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-07-01 14:13:10 +0900

    [level 0] Title: 코드 처리하기, Time: 378.96 ms, Memory: 377 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -4,19 +4,16 @@ class Solution {
         int mode= 0;
         
         for(int i=0;i<code.length();i++){
-            if(mode == 0){
-                if(code.charAt(i)!='1'){
-                    if(i%2==0) answer += code.charAt(i);
-                }else if(code.charAt(i)=='1') mode = 1;
-            }else if(mode == 1){
-                if(code.charAt(i)!='1'){
-                    if(i%2==1) answer += code.charAt(i);
-                }else if(code.charAt(i)=='1') mode = 0;
+            char current = code.charAt(i);
+            
+            if(current=='1'){
+                mode = mode==0?1:0;
+                continue;
             }
+            
+            if(i%2==mode) answer+=(current);
         }
         
-        if(answer.equals("")) answer = "EMPTY";
-        
-        return answer;
+        return answer.equals("")?"EMPTY":answer;
     }
 }
