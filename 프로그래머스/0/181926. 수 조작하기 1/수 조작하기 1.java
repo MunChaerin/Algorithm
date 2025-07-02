41202e6c70357a56f8ab3ec387a1d8f89b4f1e67
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-07-02 13:42:38 +0900

    [level 0] Title: 수 조작하기 1, Time: 58.82 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,10 +1,20 @@
 class Solution {
     public int solution(int n, String control) {
         for(String s : control.split("")){
-            if(s.equals("w")) n += 1;
-            else if(s.equals("s")) n -= 1;
-            else if(s.equals("d")) n += 10;
-            else if(s.equals("a")) n -= 10;
+            switch(s){
+                case "w" :
+                    n+=1;
+                    break;
+                case "s" :
+                    n-=1;
+                    break;
+                case "d" :
+                    n+=10;
+                    break;
+                case "a" :
+                    n-=10;
+                    break;
+            }
         }
         return n;
     }
