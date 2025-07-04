32e5c78d9fd35d221bb0c37e64d501a0aa2156cf
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-07-04 13:38:25 +0900

    [level 0] Title: 세로 읽기, Time: 17.41 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -1,12 +1,8 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
         String answer = "";
-        if(m==1){
-            answer = my_string;
-        }else{
-            for(int i=c-1;i<my_string.length();i+=m){
+        for(int i=c-1;i<my_string.length();i+=m){
                 answer += my_string.charAt(i);
-            }
         }
         return answer;
     }
