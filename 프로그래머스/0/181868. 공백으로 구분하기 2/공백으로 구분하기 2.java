0d716027144d2e9b8649dfbb5d200444085c0ca5
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-07-09 13:24:41 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 1.04 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,11 +1,6 @@
 import java.util.*;
 class Solution {
     public String[] solution(String my_string) {
-        String[] str = my_string.split(" ");
-        List<String> answer = new ArrayList<>();
-        for(String s : str){
-            if(!s.equals("")) answer.add(s);
-        }
-        return answer.toArray(new String[0]);
+        return my_string.trim().split("[ ]+");
     }
 }
