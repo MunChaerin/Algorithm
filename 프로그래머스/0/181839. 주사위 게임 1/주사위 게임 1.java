f79e7b1a4782b442d704557b9c155de451cf4a07
Chaerin <164905124+MunChaerin@users.noreply.github.com>
2025-10-29 21:24:59 +0000

    [level 0] Title: 주사위 게임 1, Time: 0.06 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int a, int b) {
-        if(a%2!=0&&b%2!=0) return (int) Math.floor(Math.pow(a*1.0,2)+Math.pow(b*1.0,2));
+        if(a%2!=0&&b%2!=0) return (int)Math.pow(a*1.0,2)+(int)Math.pow(b*1.0,2);
         else if(a%2!=0||b%2!=0) return 2*(a+b);
         else return Math.abs(a-b);
     }
